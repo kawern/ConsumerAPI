4fcbde2e130ddf748bbee9b98c6b326096e6073f
Kasper <77611053+kawern@users.noreply.github.com>
2021-06-08 12:41:19 +0200

    added style

--- src/js/createAnimal.js
+++ src/js/createAnimal.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+
 let form = document.querySelector("#createAnimal");
 
 
@@ -6,16 +8,15 @@ let submitAnimal = function(e) {
 
     const myFormData = new FormData();
     myFormData.append("type", e.target.type.value);
-    myFormData.append("breed", e.target.type.value);
-    myFormData.append("name", e.target.type.value);
-    myFormData.append("age", e.target.type.value);
-    myFormData.append("sex", e.target.type.value);
-    myFormData.append("colors", e.target.type.value);
+    myFormData.append("breed", e.target.breed.value);
+    myFormData.append("name", e.target.name.value);
+    myFormData.append("age", e.target.age.value);
+    myFormData.append("sex", e.target.sex.value);
+    myFormData.append("colors", e.target.colors.value);
 
     fetch("http://kawernapi.herokuapp.com/api/v1/animals", {
   "method": "POST",
   "headers": {
-    "Content-Type": "multipart/form-data",
     "Authorization": "Bearer 7835iyrehbsfm7523tyewfhsvdb46oitrjgdn2u3tewgndb"
   },
   "body": myFormData
@@ -25,10 +26,4 @@ let submitAnimal = function(e) {
 }
 form.addEventListener("submit", submitAnimal)
 
-
-
-// fetch("http://kawernapi.herokuapp.com/api/v1/animals", {
-//   })
-//     .then(response => response.json())
-//     .then(result => console.log(result))
-//     .catch(err => console.error(err));
+})
